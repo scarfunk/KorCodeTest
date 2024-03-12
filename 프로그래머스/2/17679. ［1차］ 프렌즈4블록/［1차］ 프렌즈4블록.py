5e28ebf05edad6f7a22422822b41ef1481440c0f
sukkyu.oh <scarfunk@naver.com>
2024-03-12 21:40:34 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 4.26 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -0,0 +1,60 @@
+
+def down(b):
+    # 아래 행부터 올라가며 빈곳을 찾으면 내린다
+    x = len(b)
+    y = len(b[0])
+    for i in range(x-1, 0, -1):
+        for j in range(0, y):
+            if b[i][j] == "":
+                # print(f'{i}{j}')
+                for k in range(i-1, -1, -1):
+                    if b[k][j] != "":
+                        # print(f'kj:{k}{j}: {b[k][j]}')
+                        b[i][j] = b[k][j]
+                        b[k][j] = ""
+                        break # 1개 내렸으면 멈춤
+    
+    # print(b)
+    return b
+
+def bomb(b):
+    cnt = 0
+    x = len(b)
+    y = len(b[0])
+    my_set = set()
+    for i in range(0, x-1):
+        for j in range(0, y-1):
+            if b[i][j] == "":
+                continue
+            if b[i][j] == b[i+1][j+1] and b[i][j] == b[i+1][j] and b[i][j] == b[i][j+1]:
+                my_set.add(tuple([i,j]))
+                my_set.add(tuple([i+1,j]))
+                my_set.add(tuple([i,j+1]))
+                my_set.add(tuple([i+1,j+1]))
+                
+    # 터트릴것들을 저장하고 터트린다.
+    # print(my_set)
+    if len(my_set) == 0:
+        return 0
+    cnt = len(my_set)
+    
+    for (x,y) in my_set:
+        b[x][y] = ""
+    # print(b)
+    b = down(b)
+    return len(my_set) + bomb(b) # 다시 재귀를 하고 터트릴게 없으면 stop 한다.
+    
+
+    
+    
+    
+
+def solution(m, n, board):
+    ret = []
+    for b in board:
+        ret.append(list(b))
+    result = bomb(ret)
+    print(result)
+    return result
+
+            
