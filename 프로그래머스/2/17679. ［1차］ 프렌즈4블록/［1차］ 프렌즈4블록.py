04fa824cff21247e9f14d2ee21b1d9736db5ee6c
sukkyu.oh <scarfunk@naver.com>
2024-03-12 21:46:52 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 7.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -5,15 +5,14 @@ def down(b):
     y = len(b[0])
     for i in range(x-1, 0, -1):
         for j in range(0, y):
-            if b[i][j] == "":
+            if b[i][j] == "": # 빈것을 찾았으면
                 # print(f'{i}{j}')
-                for k in range(i-1, -1, -1):
-                    if b[k][j] != "":
+                for k in range(i-1, -1, -1): # 열은 고정하고 행만 -1 씩 0 까지.
+                    if b[k][j] != "": 
                         # print(f'kj:{k}{j}: {b[k][j]}')
-                        b[i][j] = b[k][j]
-                        b[k][j] = ""
+                        b[i][j] = b[k][j] # 내리고 
+                        b[k][j] = "" # 타겟은 비운다.
                         break # 1개 내렸으면 멈춤
-    
     # print(b)
     return b
 
@@ -24,7 +23,7 @@ def bomb(b):
     my_set = set()
     for i in range(0, x-1):
         for j in range(0, y-1):
-            if b[i][j] == "":
+            if b[i][j] == "": # 빈거는 팡의 대상이 아니라 패스
                 continue
             if b[i][j] == b[i+1][j+1] and b[i][j] == b[i+1][j] and b[i][j] == b[i][j+1]:
                 my_set.add(tuple([i,j]))
@@ -32,29 +31,23 @@ def bomb(b):
                 my_set.add(tuple([i,j+1]))
                 my_set.add(tuple([i+1,j+1]))
                 
-    # 터트릴것들을 저장하고 터트린다.
     # print(my_set)
-    if len(my_set) == 0:
+    if len(my_set) == 0: # 터트릴게 없으면 재귀 종료
         return 0
     cnt = len(my_set)
-    
+    # 터트릴것들을 저장하고 한방에 터트린다.
     for (x,y) in my_set:
         b[x][y] = ""
     # print(b)
     b = down(b)
     return len(my_set) + bomb(b) # 다시 재귀를 하고 터트릴게 없으면 stop 한다.
-    
-
-    
-    
-    
 
 def solution(m, n, board):
     ret = []
     for b in board:
         ret.append(list(b))
     result = bomb(ret)
-    print(result)
+    # print(result)
     return result
 
             
